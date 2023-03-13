e458abef66535da3c21b69741d9d31826e1839d4
henriqueambrosano <henriqueambrosano@hotmail.com>
2023-03-13 19:16:04 -0300

    refactor: adjusting path

--- src/components/icons/CardElement.vue
+++ src/components/icons/CardElement.vue
@@ -1,7 +1,7 @@
 <template>
   <div :id="card.id" :class="elementClass" @click="flipCard(card.id)">
     <div class="card_front">
-      <img :src="`/public/static/${card.icon}.png`" class="icon" alt="game-card"/>
+      <img :src="`/static/${card.icon}.png`" class="icon" alt="game-card"/>
     </div>
     <div class="card_back">&lt;/&gt;</div>
   </div>
